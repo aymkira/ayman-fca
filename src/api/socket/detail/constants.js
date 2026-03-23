04f89f6669d234d6c9b4715380dbee61d10efecc
ayman <kirabotayman@gmail.com>
2026-03-23 21:37:15 +0300

    Add files via upload

---
+++ src/api/socket/detail/constants.js
@@ -0,0 +1,29 @@
+// ============================================================
+//  AYMAN-FCA v2.0 — MQTT Topics
+//  © 2025 Ayman. All Rights Reserved.
+// ============================================================
+"use strict";
+
+module.exports = {
+  topics: [
+    "/ls_req",
+    "/ls_resp",
+    "/legacy_web",
+    "/webrtc",
+    "/rtc_multi",
+    "/onevc",
+    "/br_sr",
+    "/sr_res",
+    "/t_ms",
+    "/thread_typing",
+    "/orca_typing_notifications",
+    "/notify_disconnect",
+    "/orca_presence",
+    "/inbox",
+    "/mercury",
+    "/messaging_events",
+    "/orca_message_notifications",
+    "/pp",
+    "/webrtc_response"
+  ]
+};
